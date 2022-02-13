2b911564a37e15dfd08360698f36a37d70ea4849
WaReZoR <v.n.oleynichenko@gmail.com>
2022-02-13 20:47:09 +0200

    исправления оносительно clamp и не отработка стилей

--- dist/js/app.js
+++ dist/js/app.js
@@ -200,10 +200,8 @@
                 if (!el.closest("[data-spollers]")) spollersClose.forEach((spollerClose => {
                     const spollersBlock = spollerClose.closest("[data-spollers]");
                     const spollerSpeed = spollersBlock.dataset.spollersSpeed ? parseInt(spollersBlock.dataset.spollersSpeed) : 500;
-                    if (!spollersBlock.querySelectorAll("._slide").length) {
-                        spollerClose.classList.remove("_spoller-active");
-                        _slideUp(spollerClose.nextElementSibling, spollerSpeed);
-                    }
+                    spollerClose.classList.remove("_spoller-active");
+                    _slideUp(spollerClose.nextElementSibling, spollerSpeed);
                 }));
             }));
         }
